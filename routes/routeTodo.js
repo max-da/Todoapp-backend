84aa02117b3d5e213155a13faf443d7197c57c96
max-da <max.dahlbom@outlook.com>
2021-01-26 15:40:20 +0100

    changes

--- routes/routeTodo.js
+++ routes/routeTodo.js
@@ -5,7 +5,7 @@ const Todo = require("../models/todoSchema");
 router.get("/", async (req, res)=>{
     try {
         const data = await Todo.find()
-        res.render("index.ejs", {data:data, error:""})
+        res.render("index.ejs", {data:data, error:"", Todoedit: ""})
     }
     catch(err) {
        console.log(err)
@@ -25,9 +25,39 @@ router.post("/addData", async (req, res)=> {
         console.log(err)
         const error = "Please enter a todo before submitting"
         const data = await Todo.find()
-        res.render("index.ejs", {data:data, error: error})
+        const Todoedit = "";
+        res.render("index.ejs", {data:data, error: error, Todoedit:Todoedit})
      }
     
 })
 
+router.get("/delete/:id",async (req, res)=> {
+    await Todo.deleteOne({_id:req.params.id})
+    res.redirect("/")
+})
+
+router.get("/edit/:id", async(req, res)=> {
+ try  { const Todoedit = await Todo.findOne({_id:req.params.id})
+    let error =" "
+    const data = await Todo.find()
+    res.render("index.ejs",{Todoedit:Todoedit, error:error, data:data})}
+    catch(err){
+        console.log(err)
+        const error = "Please enter a todo before submitting"
+        const data = await Todo.find()
+        res.render("index.ejs", {data:data, error: error})
+    }
+})
+
+router.post("/edit", async (req, res)=> {
+ try  { await Todo.updateOne({_id:req.body.id},{
+        name:req.body.name
+    })}
+  
+    catch(err){
+console.log(err)
+    }
+    console.log(req.body)
+    res.redirect("/")
+})
 module.exports = router;
